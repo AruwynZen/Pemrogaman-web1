860394548bd87cc28a2cc0cdb447b20e9b677c10
Aruwyn <budisuryawan96@gmail.com>
2020-04-05 12:30:13 +0800

    Javascrpt Part 1

---
+++ Javascript/materi/javascript/script.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+    var my_function = function (event) {
+        alert("Hello Mahasiswa Primakara");
+        event.preventDefault();
+    };
+
+    var form = document.getElementById("cart-hplus");
+
+    form.addEventListener("submit", my_function, true);
+
+
+})();
